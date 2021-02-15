9f77d9f755dc7c44035f9ebf2be1ea4af0cc73f5
jason-kills-u <75362028+jason-kills-u@users.noreply.github.com>
2021-02-15 18:47:07 +0700

    Update libs.hpp

--- bin/include/libs.hpp
+++ bin/include/libs.hpp
@@ -7,69 +7,20 @@
 void interpret(std::string G)
 {
 	int B = 65;
-	bool i = false;
 	std::string T, T2;
-	bool i2 = false;
 	for(int i = 0; i < G.length(); i++)
 	{
 		if(G[i] == '+')
 		{
-			if(i == true)
-			{
-				T.push_back(G[i]);
-			}
-			if(i2 == true)
-			{
-				T2.push_back(G[i]);
-			}
 			B++;
 		}
 		else if(G[i] == '-')
 		{
-			if(i == true)
-			{
-				T.push_back(G[i]);
-			}
-			if(i2 == true)
-			{
-				T2.push_back(G[i]);
-			}
 			B--;
 		}
 		else if(G[i] == '.')
 		{
-			if(i == true)
-			{
-				T.push_back(G[i]);
-			}
 			std::cout << char(B);
-		}
-		else if(G[i] == '<')
-		{
-			i = true;
-		}
-		else if(G[i] == '>' && i == true)
-		{
-			i2 = true;
-		}
-		else if(G[i] == '[')
-		{
-			int res;
-			for(int i = 0; i < T2.length(); i++)
-			{
-				if(T2[i] == '+')
-				{
-					res++;
-				}
-				else if(T2[i] == '-')
-				{
-					res--;
-				}
-			}
-			for(int i = 0; i < res; i++)
-			{
-				interpret(T);
-			}
 		}
 	}
 }
